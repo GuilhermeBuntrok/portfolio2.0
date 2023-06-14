24933f22768871a9bd6d2b873d556779a588ba17
GuilhermeBuntrok <guilhermebuntrock@hotmail.com>
2023-06-14 13:44:40 -0300

    :sparkles: feat: Query GraphQL Hygraph

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,49 @@
+import { KnownTechs } from './components/known-techs'
+import { HeroSection } from './components/pages/home/hero-section'
+import { HighLightedProjects } from './components/pages/home/highlighted-projectrs'
+import { WorkExperience } from './components/work-experience'
+import { fetchHygraphQuery } from './utils/fetch-hygraph-query'
+
+const getPageData = async () => {
+  const query = `
+  query PageInfoQuery {
+    page(where: {slug: "home"}) {
+      introduction {
+        raw
+      }
+      technologies {
+        name
+      }
+      profilePicture {
+        url
+      }
+      socials {
+        url
+        iconSvg
+      }
+      knownTechs {
+        iconSvg
+        name
+        startDate
+      }
+      
+    }
+  }
+`
+
+  return fetchHygraphQuery(query)
+}
+
 export default async function Home() {
+  const response = await getPageData()
+
+  console.log(response)
   return (
     <>
-      <h1>Home Page</h1>
+      <HeroSection />
+      <KnownTechs />
+      <HighLightedProjects />
+      <WorkExperience />
     </>
   )
 }
